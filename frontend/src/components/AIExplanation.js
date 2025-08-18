792bf4936f8a9ec891a48e982cdf8cc0e8b5e32c
zohra <zsohrabi15@gmail.com>
2025-08-18 10:13:45 +0100

    Add shadow to AI Explanation functionality

--- frontend/src/components/AIExplanation.js
+++ frontend/src/components/AIExplanation.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const AIExplanation = ({ explanation }) => {
   return (
-    <div className="pt-4 border border-grey rounded-xl">
+    <div className="pt-4 border border-grey shadow-md rounded-xl">
       <h4 className="font-semibold">AI Explanation</h4>
       <p className="p-2 text-sm lg:text-base lg:p-6">{explanation}</p>
     </div>
