67585ece5f42524d0d713530f14228ebc825d8a8
zohra <zsohrabi15@gmail.com>
2025-09-02 09:56:11 +0100

    Design change

--- frontend/src/components/AIExplanation.js
+++ frontend/src/components/AIExplanation.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const AIExplanation = ({ explanation }) => {
   return (
-    <div className="pt-4 border border-grey shadow-md rounded-xl">
-      <h4 className="font-semibold">AI Explanation</h4>
+    <div className="pt-4 border border-black bg-black/25 shadow-md rounded-xl">
+      <h4 className="font-semibold text-white">AI Explanation</h4>
       <p className="p-2 text-sm lg:text-base lg:p-6">{explanation}</p>
     </div>
   );
