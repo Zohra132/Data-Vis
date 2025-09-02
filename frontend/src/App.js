4896770b3bf0b9ac1b0bb8b0849fc35ce78169f5
zohra <zsohrabi15@gmail.com>
2025-09-02 10:00:20 +0100

    Wrap'App.js' with 'AuthContext' provider

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import React from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate} from "react-router-dom";
+import React, {useContext} from "react";
 import Home from "./components/Home"; 
-
 import Stack from "./components/Stack/Stack";
 import LLStack from "./components/LLStack/LLStack";
 import Queue from "./components/Queue/Queue";
@@ -11,23 +10,41 @@ import List  from "./components/List/List";
 import Heap  from "./components/Heap/Heap";
 import BST from "./components/BST/BST";
 import BubbleSort from "./components/BubbleSort/BubbleSort";
+import AuthForm from "./components/Navbar/AuthForm";
+import Account from "./components/Account/Account";
+import { AuthProvider, AuthContext  } from "./context/AuthContext";
+
+function AppRoutes() {
+  const { user } = useContext(AuthContext);
+
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/Stack" element={<Stack />} />
+      <Route path="/LLStack" element={<LLStack />} />
+      <Route path="/Queue" element={<Queue />} />
+      <Route path="/Array" element={<Array />} />
+      <Route path="/SinglyLinkedList" element={<SinglyLinkedList />} />
+      <Route path="/List" element={<List />} />
+      <Route path="/Heap" element={<Heap />} />
+      <Route path="/BinarySearchTree" element={<BST />} />
+      <Route path="/BubbleSort" element={<BubbleSort/>} />
+      <Route path="/AuthForm" element={<AuthForm/>} />
+      <Route 
+        path="/Account" 
+        element={user ? <Account /> : <Navigate to="/authForm" replace />}
+      />
+    </Routes>
+  );
+}
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/Stack" element={<Stack />} />
-        <Route path="/LLStack" element={<LLStack />} />
-        <Route path="/Queue" element={<Queue />} />
-        <Route path="/Array" element={<Array />} />
-        <Route path="/SinglyLinkedList" element={<SinglyLinkedList />} />
-        <Route path="/List" element={<List />} />
-        <Route path="/Heap" element={<Heap />} />
-        <Route path="/BinarySearchTree" element={<BST />} />
-        <Route path="/BubbleSort" element={<BubbleSort/>} />
-      </Routes>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <AppRoutes />
+      </Router>
+    </AuthProvider>
   );
 }
 
